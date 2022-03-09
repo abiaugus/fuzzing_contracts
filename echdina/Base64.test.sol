d30e5fb513400d1967896b29035558b78c78a4b4
Abhijith <abhi@Abhijiths-MacBook-Air.local>
2022-03-09 12:45:19 +0530

    re-structuring

--- echdina/Base64.test.sol
+++ echdina/Base64.test.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import './Base64.sol';
+import '../Base64.sol';
 
 //incomplete
 
@@ -8,15 +8,17 @@ contract Test {
 
     bool flag = true;
     bool compare_flag = true;
+    bool flag2 = true;
 
-    function test_encode (string memory _str) public {
-        bytes memory _bs = bytes(_str);
-        uint256 rem = _bs.length % 3;
-        uint256 res_length = (_bs.length + 2) / 3 * 4 - ((3 - rem) % 3);
-        string memory res1 = new string(res_length);
-        res1 = Base64.encode(_str);
-        string memory test_string = "";
-        if((keccak256(abi.encodePacked(res1))) == keccak256(abi.encodePacked(test_string))){
+    function test_encode () public {
+        // bytes memory _bs = bytes(_str);
+        // uint256 rem = _bs.length % 3;
+        // uint256 res_length = (_bs.length + 2) / 3 * 4 - ((3 - rem) % 3);
+        string memory res1;
+        string memory res2;
+        res1 = Base64.encode("hello");
+        res2 = Base64.encode("hello");
+        if((keccak256(abi.encodePacked(res1))) != keccak256(abi.encodePacked(res2))){
             flag = false;
         }
     }
@@ -48,14 +50,21 @@ contract Test {
             compare_flag = false;
         }
     }
+    function compare_encode() public {
+        string memory str = "MTIzdGVzdEJhJGVEZWNvZGU";
+        string memory res = Base64.encode("123testBa$eDecode");
+        if((keccak256(abi.encodePacked(str))) != keccak256(abi.encodePacked(res))){
+            compare_flag = false;
+        }
+    }
 
     function echidna_test_encode() public view returns (bool){
         return flag;
     }
     function echidna_test_compare_encodes() public view returns (bool){
         return compare_flag;
-    }
-    function echidna_alwaystrue() public returns (bool){
-        return(true);
-  }
+    }   
+    function echidna_test_compare_encode() public view returns (bool){
+        return flag2;
+    } 
 }
--- echdina/LitVerify.test.sol
+++ echdina/LitVerify.test.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.0;
 
-import './LitVerify.sol';
+import '../LitVerify.sol';
 //incomplete
 contract Test {
     
